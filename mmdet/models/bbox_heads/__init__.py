af196d53f574cd6c3d14211f035df26d31454c4f
scai <scai@cmri.org.au>
2019-12-24 14:27:25 +1100

    add support to replace classifier with regressor

--- mmdet/models/bbox_heads/__init__.py
+++ mmdet/models/bbox_heads/__init__.py
@@ -1,7 +1,10 @@
 from .bbox_head import BBoxHead
+from .bbox_head_reg import BBoxHeadReg
 from .convfc_bbox_head import ConvFCBBoxHead, SharedFCBBoxHead
 from .double_bbox_head import DoubleConvFCBBoxHead
+from .convfc_bbox_head_reg import ConvFCBBoxHeadReg, SharedFCBBoxHeadReg
 
 __all__ = [
-    'BBoxHead', 'ConvFCBBoxHead', 'SharedFCBBoxHead', 'DoubleConvFCBBoxHead'
+    'BBoxHead', 'ConvFCBBoxHead', 'SharedFCBBoxHead', 'DoubleConvFCBBoxHead', 'BBoxHeadReg', 'ConvFCBBoxHeadReg',
+    'SharedFCBBoxHeadReg'
 ]
